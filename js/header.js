f3962ea35faf3bf7aeb5859a41030f4685dd6181
ms <ms@tnetkorea.com>
2022-08-24 14:39:14 +0900

    - 버튼 이벤트 추가

--- js/header.js
+++ js/header.js
@@ -1,16 +1,18 @@
 $(function() {
-
-    $('#header-download-btn').click(function () {
+    // Download Button
+    $('#header-download-btn')
+    .on("click", "", function () {
         $('#header-download-btn ul').toggleClass('show');
-    });
-    $('#section-download-btn').click(function () {
-        $('#section-download-btn ul').toggleClass('show');
+    })
+    .on("mouseleave", "", function(){
+        $('#header-download-btn ul').removeClass('show');
     });
 
-    // Mobile Navigation
-    $('.navbar-toggler').click(function () {
+    // GNB
+    $('.navbar-toggler').on("click", "", function(){
         $(this).toggleClass('show');
         $('.header').toggleClass('active'); // Set Header Background Color
         $('.navbar ul').toggleClass('show');
-    });
+    })
+
 })
